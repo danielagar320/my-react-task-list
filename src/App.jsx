2a6846666c267754ed1bd62519fbff8802e99234
danielagar320 <daniela.garcia-r@mail.escuelaing.edu.co>
2023-10-02 23:14:09 -0500

    Falta eliminar y actualizar

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,13 @@ import Header from "./components/Header";
 import { TaskList } from "./components/TaskList";
 import "./app.css"
 
-const latestTask = [
-  {name: "Buy a new gaming laptop"},
-  {name: "Complete a previous task"},
-  {name: "Create video for youtube"},
-  {name: "Create a new portfolio"}
-]
+
 
 function App() {
   return ( 
       <div className="App">
         <Header/>
-        <TaskList list={latestTask}/>
+        <TaskList/>
       </div>  
   );
 }
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,28 +1,44 @@
+import React, { useState } from "react";
 import { Task } from "./Task";
 
-export const TaskList = (props) =>{
+export const TaskList = (props) => {
+  const [taskInput, setTaskInput] = useState(""); 
+  const [list, setList] = useState(props.list || []); 
 
-    function handleSubmit(e){
-        e.preventDefault;
+  const handleInputChange = (e) => {
+    setTaskInput(e.target.value);
+  };
 
+  const addTask = () => {
+    if (taskInput.trim() !== "") {
+      setList([...list, { name: taskInput }]);
+      setTaskInput(""); // Limpiar el campo de entrada después de agregar una tarea
     }
+  };
 
-    const{list} = props;
-    return(
-        <div>
-            <div className="textbox">
-                <input  type="text" placeholder="Add your new todo"/>
-                <button>Add</button>
-            </div>
+  const deleteTask = (index) => {
+    const updatedList = [...list];
+    updatedList.splice(index, 1);
+    setList(updatedList);
+  };
 
-            <ul>
-                {list.map((task, index) => (
-                <Task key={index} name={task.name} />
-            ))}
-            </ul>
+  return (
+    <div>
+      <div className="textbox">
+        <input
+          type="text"
+          placeholder="Add your new todo"
+          value={taskInput}
+          onChange={handleInputChange}
+        />
+        <button onClick={addTask}>Add</button>
+      </div>
 
-        </div>
-
-    );
-
-}
+      <ul>
+        {list.map((task, index) => (
+          <Task key={index} name={task.name} />
+        ))}
+      </ul>
+    </div>
+  );
+};
